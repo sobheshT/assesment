df27428e49a29faf45c9fcf991dcbf237db6caf1
Sobhesh Tripathy <sobheshbarun@gmail.com>
2019-07-24 13:31:43 +0530

    assesment rework

---
+++ assesment-Eshop/dbs/product.js
@@ -0,0 +1,53 @@
+const product = [
+
+    {
+        id : 1,
+        name : 'Iphone X',
+        brand : 'Apple',
+        quantity : '1',
+        price : '1500000/-',
+
+    },
+
+    {
+        id : 2,
+        name : 'FITBIT',
+        brand : 'amazefit',
+
+        quantity : '1',
+        
+        price : '800',
+    },
+
+    {
+        id : 3,
+
+        name : 'Laptop',
+
+        brand : 'Lenovo',
+
+        quantity : '1',
+
+        price :  '50000',
+
+
+
+    },
+    {
+        id : 4,
+
+        name : 'Sprite',
+    
+        brand : 'Coca Cola',
+    
+        quantity : '1',
+    
+        price :  '50',
+    }
+    
+
+
+]
+
+module.exports=product
+
